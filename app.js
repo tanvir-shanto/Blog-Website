eddc2cd0c535aa15ec0545ed8e41450b89c840ca
Tanvir Ahmed Shanto <tashanto@mun.ca>
2023-09-14 21:10:15 -0230

    Included the ejs layout middleware

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const expressLayout = require('express-ejs-layouts');
+
 const PORT = 5000 || process.env.PORT;
 
-app.get("", (req, res) => {
-    res.send("Hello World");
-});
+app.use(express.static('public'));
+
+app.use(expressLayout); // Use the ejs layout middleware
+app.set('layout', './layouts/main'); // Set the main layout for the app
+app.set('view engine', 'ejs'); // Set the ejs view engine
+
+app.use("/", require('./server/routes/main'));
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
