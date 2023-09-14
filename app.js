4476149f776385ee32f8e26c557eac52bdf6b461
Tanvir Ahmed Shanto <tashanto@mun.ca>
2023-09-14 20:46:38 -0230

    Initial stage. Displayed Hello World in localhost

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = 5000 || process.env.PORT;
+
+app.get("", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+})
