996466434c340e091464cbdf053b8173f3c27ec6
Tanvir Ahmed Shanto <tashanto@mun.ca>
2023-09-15 18:21:21 -0230

    Added stylesheet and script and tested them

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get("", (req, res) => {
-    res.render('index');
+
+    const locals = {
+        title: "Blog Website",
+        description: "Blog Website created with Node, Express and MongoDB"
+    }
+
+    res.render('index', locals);
 });
 
 router.get("/about", (req, res) => {
