3aa683295b468dd356afb83e7c4caaa338e3a9ad
Tanvir Ahmed Shanto <tashanto@mun.ca>
2023-09-26 03:52:52 -0230

    Implemented admin login using bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require('express');
 const app = express();
 
 const expressLayout = require('express-ejs-layouts');
-const connectDB = require('./server/config/db')
+const connectDB = require('./server/config/db');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const PORT = 5000 || process.env.PORT;
 
@@ -13,6 +16,17 @@ connectDB();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  }));
 
 app.use(express.static('public'));
 
